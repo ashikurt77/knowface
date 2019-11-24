bef1e07d1f0977902e9655dca1e6a5bc7bdc79ac
Ashiqur Tusher <ashikurt77@gmail.com>
2019-11-24 10:17:30 +0600

    Updates

---
+++ precache-manifest.b40b2b8cf39a9518e4a18ff5634b894f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa1a658b4e0fd4dff96d47d11e0a09b",
+    "url": "/knowface/index.html"
+  },
+  {
+    "revision": "687560c75aa759eeb498",
+    "url": "/knowface/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a1dc9c3f20373258c7f7",
+    "url": "/knowface/static/css/main.695bc034.chunk.css"
+  },
+  {
+    "revision": "687560c75aa759eeb498",
+    "url": "/knowface/static/js/2.51188545.chunk.js"
+  },
+  {
+    "revision": "a1dc9c3f20373258c7f7",
+    "url": "/knowface/static/js/main.aca3b0d7.chunk.js"
+  },
+  {
+    "revision": "e60679bf7b512ff0c02c",
+    "url": "/knowface/static/js/runtime~main.5e9fa07c.js"
+  },
+  {
+    "revision": "f27d26fc132e3df6fd61d325f394ae8a",
+    "url": "/knowface/static/media/face.f27d26fc.png"
+  }
+]);
